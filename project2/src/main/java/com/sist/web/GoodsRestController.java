a8fd0e56f2b55e62d4d83e34e860a2892282738c
eunyooung <choi327391@gmail.com>
2022-03-31 00:32:27 +0900

    상세정보 오류 수정중

--- project2/src/main/java/com/sist/web/GoodsRestController.java
+++ project2/src/main/java/com/sist/web/GoodsRestController.java
@@ -14,28 +14,30 @@ import com.sist.vo.GoodsVO;
 @RestController
 
 public class GoodsRestController {
-	@Autowired
-	private GoodsService service;
+    @Autowired
+    private GoodsService service;
 
-	 @GetMapping(value="goods/detail_vue.do",produces ="text/plain;charset=utf-8")
-	   public String goods_detail()
-	   {
-		   Map map=new HashMap();
-		   GoodsVO vo=service.goodsDetail(map);
-		 
-		   JSONObject obj=new JSONObject();
-		   obj.put("gid", vo.getG_id());
-		   obj.put("name", vo.getG_name());
-		   obj.put("brand", vo.getG_brand());
-		   obj.put("price", vo.getG_price());
-		   obj.put("sale", vo.getG_sale());
-		   obj.put("image", vo.getG_image());
-		   obj.put("detail", vo.getG_detail());
-		   obj.put("stock", vo.getG_stock());
-		   obj.put("sold", vo.getG_sold());
-		   obj.put("status", vo.getG_status());
-		   obj.put("regdate", vo.getG_regdate());
-		   
-		   return obj.toJSONString();
-	   }
+    @GetMapping(value = "goods/detail_vue.do", produces = "text/plain;charset=utf-8")
+    public String goods_detail(String gid) {
+        System.out.println(gid);
+        Map map = new HashMap();
+        map.put("g_id", gid);
+        GoodsVO vo = service.goodsDetail(map);
+
+        JSONObject obj = new JSONObject();
+        
+        obj.put("gid", vo.getG_id());
+        obj.put("name", vo.getG_name());
+        obj.put("brand", vo.getG_brand());
+        obj.put("price", vo.getG_price());
+        obj.put("sale", vo.getG_sale());
+        obj.put("image", vo.getG_image());
+        obj.put("detail", vo.getG_detail());
+        obj.put("stock", vo.getG_stock());
+        obj.put("sold", vo.getG_sold());
+        obj.put("status", vo.getG_status());
+        obj.put("regdate", vo.getG_regdate());
+        
+        return obj.toJSONString();
+    }
 }
