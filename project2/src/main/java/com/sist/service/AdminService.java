adc3f529d705ec17884f7e09cd02d3e3e32ebd7a
future <83049262+future-mi@users.noreply.github.com>
2022-04-04 22:00:53 +0900

    Order/Order_detail mapper,dao 수정 orderdetail.jsp 추가 등

--- project2/src/main/java/com/sist/service/AdminService.java
+++ project2/src/main/java/com/sist/service/AdminService.java
@@ -7,17 +7,11 @@ import com.sist.vo.OrderDetailVO;
 import com.sist.vo.OrderVO;
 
 public interface AdminService {
+	
 	public List<GoodsVO> goodsTotalList(Map map);
 	public int goodsTotalPage();
 	
-	
-	
-	
-	/* Order 영역  */
-	public List<OrderVO> orderTotalList(Map map);
-	public int orderTotalPage();
 	public List<OrderDetailVO> orderDetailList(Map map);
-    public List<OrderDetailVO> orderDetail(OrderDetailVO vo, int o_id);
-	
-	
+	List<OrderDetailVO> orderDetail(String oid);
+	List<String> nameData(String oid);
 }
