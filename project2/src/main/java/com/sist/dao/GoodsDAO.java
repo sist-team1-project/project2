4eb4381b8555c3ea5b063a87976cabda44a759e3
smincy <smincy@naver.com>
2022-04-12 22:21:01 +0900

    관리자 - 상품수정 이벤트부분 수정

--- project2/src/main/java/com/sist/dao/GoodsDAO.java
+++ project2/src/main/java/com/sist/dao/GoodsDAO.java
@@ -13,109 +13,112 @@ import com.sist.mapper.*;
 @Repository
 public class GoodsDAO {
 
-    @Autowired
-    private GoodsMapper mapper;
+	@Autowired
+	private GoodsMapper mapper;
 
-    @Autowired
-    private EventGoodsMapper mapper2;
-    
-    /*  ------- 리스트 페이지 -------  */
-    public String goodsMaxPrice(String cid) {
-        return mapper.goodsMaxPrice(cid);
-    }
+	@Autowired
+	private EventGoodsMapper mapper2;
 
-    public List<Map<String,Object>> goodsList(Map map) {
-        return mapper.goodsList(map);
-    }
-    
-    public int goodsListTotalpage(Map map) {
-        return mapper.goodsListTotalpage(map);
-    }
-    
-    public List<String> brandList(String cid) {
-        return mapper.brandList(cid);
-    }
-    /*  --------------------------  */
-    
-    /*  ------- 제품 상세 -------  */
-    public int goodsStatus(int gid) {
-        return mapper.goodsStatus(gid);
-    }
-    
-    public Map<String,Object> goodsDetail(Map map) {
-        return mapper.goodsDetail(map);
-    }
-    /*  --------------------------  */
-    
-    /*  ------- 관리자  -------  */
-    public List<Map<String,Object>> adminGoodsFind(Map map) {
-        return mapper.adminGoodsFind(map);
-    }
-    
-    public int adminGoodsTotalPage(Map map) {
-        return mapper.adminGoodsTotalPage(map);
-    }
+	/*  ------- 리스트 페이지 -------  */
+	public String goodsMaxPrice(String cid) {
+		return mapper.goodsMaxPrice(cid);
+	}
 
-    public int adminGoodsCount() {
-        return mapper.adminGoodsCount();
-    }
-    /*  --------------------------  */
-    public GoodsVO adminGoodsDetail(int gid) {
-        return mapper.adminGoodsDetail(gid);
-    }
-    
-    
-    /*  ------- 상품 등록 -------  */
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
-    public void goodsInsert(GoodsVO vo, String eid) {
-        
-        mapper.goodsInsert(vo); // Insert하고 SelectKey로 vo.g_id에 시퀀스 값을 넣어줌
-        
-        int gid = vo.getG_id();
-        
-        if(eid != "") {
-            StringTokenizer st = new StringTokenizer(eid,",");
-            while(st.hasMoreTokens()){
-                EventGoodsVO evo = new EventGoodsVO();
-                evo.setE_id(Integer.parseInt(st.nextToken()));
-                evo.setG_id(gid);
-                
-                mapper2.goodsEventInsert(evo);
-            }
-        }
-    }
-    
-    /* ---------------------- 관리자 상품 수정  ----------------------------  */
+	public List<Map<String, Object>> goodsList(Map map) {
+		return mapper.goodsList(map);
+	}
+
+	public int goodsListTotalpage(Map map) {
+		return mapper.goodsListTotalpage(map);
+	}
+
+	public List<String> brandList(String cid) {
+		return mapper.brandList(cid);
+	}
+	/*  --------------------------  */
+
+	/*  ------- 제품 상세 -------  */
+	public int goodsStatus(int gid) {
+		return mapper.goodsStatus(gid);
+	}
+
+	public Map<String, Object> goodsDetail(Map map) {
+		return mapper.goodsDetail(map);
+	}
+	/*  --------------------------  */
+
+	/*  ------- 관리자  -------  */
+	public List<Map<String, Object>> adminGoodsFind(Map map) {
+		return mapper.adminGoodsFind(map);
+	}
+
+	public int adminGoodsTotalPage(Map map) {
+		return mapper.adminGoodsTotalPage(map);
+	}
+
+	public int adminGoodsCount() {
+		return mapper.adminGoodsCount();
+	}
+
+	/*  --------------------------  */
+	public GoodsVO adminGoodsDetail(int gid) {
+		return mapper.adminGoodsDetail(gid);
+	}
+
+	/*  ------- 상품 등록 -------  */
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
-	public void goodsUpdate(GoodsVO vo, String eid) {
-		mapper.goodsupdate(vo);
-		int g_id = vo.getG_id();
-		List<EventGoodsVO> list = mapper2.goodsEidData(g_id);
-		for (int i = 0; i < list.size(); i++) {
-			EventGoodsVO evo = new EventGoodsVO();
-			evo.setE_id(list.get(i).getE_id());
-			evo.setG_id(g_id);
-			mapper2.goodsEventDelete(evo);
-		}
+	public void goodsInsert(GoodsVO vo, String eid) {
+
+		mapper.goodsInsert(vo); // Insert하고 SelectKey로 vo.g_id에 시퀀스 값을 넣어줌
+
+		int gid = vo.getG_id();
 
 		if (eid != "") {
 			StringTokenizer st = new StringTokenizer(eid, ",");
 			while (st.hasMoreTokens()) {
 				EventGoodsVO evo = new EventGoodsVO();
 				evo.setE_id(Integer.parseInt(st.nextToken()));
-				evo.setG_id(g_id);
+				evo.setG_id(gid);
 
 				mapper2.goodsEventInsert(evo);
 			}
 		}
 	}
-	
-    public List<EventGoodsVO> goodsEidData(int g_id) {
-        return mapper2.goodsEidData(g_id);
-    }
-    
-    /* ---------------------- 결제 페이지  ----------------------------  */
-    public List<Map<String,Object>> checkOutGoodsDetail(Map map) {
-        return mapper.checkOutGoodsDetail(map);
-    }
+
+	/* ---------------------- 관리자 상품 수정  ----------------------------  */
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public void goodsUpdate(GoodsVO vo, String eid) {
+		mapper.goodsupdate(vo);
+		if (eid != null || eid != "0") {
+			int g_id = vo.getG_id();
+			List<EventGoodsVO> list = mapper2.goodsEidData(g_id);
+			for (int i = 0; i < list.size(); i++) {
+				EventGoodsVO evo = new EventGoodsVO();
+				evo.setE_id(list.get(i).getE_id());
+				evo.setG_id(g_id);
+				mapper2.goodsEventDelete(evo);
+			}
+
+			if (eid != "") {
+				StringTokenizer st = new StringTokenizer(eid, ",");
+				while (st.hasMoreTokens()) {
+					EventGoodsVO evo = new EventGoodsVO();
+					evo.setE_id(Integer.parseInt(st.nextToken()));
+					evo.setG_id(g_id);
+
+					mapper2.goodsEventInsert(evo);
+				}
+			}
+		}
+
+	}
+
+	public List<EventGoodsVO> goodsEidData(int g_id) {
+		return mapper2.goodsEidData(g_id);
+	}
+
+	/* ---------------------- 결제 페이지  ----------------------------  */
+	public List<Map<String, Object>> checkOutGoodsDetail(Map map) {
+		return mapper.checkOutGoodsDetail(map);
+	}
 }
