062b8e39f5a0393700eedc3192f2e2b47b09f72b
M0young <sacox17@naver.com>
2022-04-15 11:47:16 +0900

    주석 추가

--- project2/src/main/java/com/sist/mapper/AskMapper.java
+++ project2/src/main/java/com/sist/mapper/AskMapper.java
@@ -8,7 +8,7 @@ import com.sist.vo.*;
 
 public interface AskMapper {
     
-    /****************** 고객센터 ******************/
+    /****************** 1:1문의 사용자 ******************/
 	@Select("SELECT a_id,u_id,a_type,a_title,a_regdate,a_group_id,a_group_tab,a_group_step "
 	        + "FROM (SELECT a_id,u_id,a_type,a_title,a_regdate,a_group_id,a_group_tab,a_group_step,rownum as num "
 	        + "FROM (SELECT a_id,u_id,a_type,a_title,TO_CHAR(a_regdate,'YYYY-MM-DD') as a_regdate,a_group_id,a_group_tab,a_group_step "
@@ -25,7 +25,7 @@ public interface AskMapper {
 	public int askTotalPage(Map map);
 	
 	
-	/****************** 관리자 ******************/
+	/****************** 1:1문의 관리자 ******************/
 	@Select("SELECT a_id,u_id,a_type,a_title,TO_CHAR(a_regdate,'YYYY-MM-DD') as a_regdate,"
 			+"a_group_id,a_group_tab, a_group_step, num "
 			+"FROM (SELECT a_id,u_id,a_type,a_title,a_regdate,a_group_id,a_group_tab, a_group_step, rownum as num "
@@ -39,21 +39,25 @@ public interface AskMapper {
 	
 	
 	/****************** 공통 ******************/
+	/* 1:1문의 추가 */
 	@Insert("INSERT INTO ask_1(a_id,u_id,a_type,a_title,a_content,a_group_id) "
 			+"VALUES(ask_id_seq_1.nextval,#{u_id},#{a_type},#{a_title},#{a_content},"
 			+"(SELECT NVL(MAX(a_group_id)+1,1) FROM ask_1))")
 	public void askInsert(AskVO vo);
-
+	
+	/* 1:1문의 상세 */
 	@Select("SELECT a_id,a_type,a_title,a_content,"
 			+"TO_CHAR(a_regdate,'YYYY-MM-DD') as a_regdate,a_group_id,a_group_step,a_group_tab "
 			+"FROM ask_1 "
 			+"WHERE a_id=#{a_id}")
 	public AskVO askDetailData(int aid);
 
+	/* 1:1문의 문의갯수 */
 	@Select("SELECT COUNT(*) FROM ask_1 "
 			+"WHERE a_group_id=#{a_group_id} AND a_group_step=1")
 	public int askCount(int a_group_id);
 
+	/* 1:1문의 문의정보 */
 	@Select("SELECT a_group_id,a_group_step,a_group_tab "
 			+"FROM ask_1 "
 			+"WHERE a_id=#{a_id}")
@@ -66,18 +70,22 @@ public interface AskMapper {
 			+"#{a_group_id},#{a_group_step},#{a_group_tab})")
 	public void askReplyInsert(AskVO vo);
 
+	/* 1:1문의 문의삭제 */
 	@Delete("DELETE FROM ask_1 "
 			+"WHERE a_group_id=#{a_group_id}")
 	public void askDelete1(int a_group_id);
 	
+	/* 1:1문의 답글삭제 */
 	@Delete("DELETE FROM ask_1 "
 			+"WHERE a_id=#{a_id}")
 	public void askDelete2(int aid);
 	
+	/* 1:1문의 문의답변 탭증가 */
 	@Update("UPDATE ask_1 SET a_group_tab = #{a_group_tab}+1 "
 			+ "WHERE a_id = #{a_id} ")
 	public void asktabReply(AskVO vo);
 	
+	/* 1:1문의 유저정보 확인 */
 	@Select("SELECT u_id FROM ask_1 WHERE a_id=#{a_id}")
 	public String checkUser(int a_id);
 }
